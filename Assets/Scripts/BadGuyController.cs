60042119f747b7358232237d9cd5a8d908790236
JotKa <teronal@gmail.com>
2018-03-09 21:26:06 +0100

    First enemy object and default renderer for enemies

---
+++ Assets/Scripts/BadGuyController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGuyController : MonoBehaviour {
+
+    public float BadGuyLife;
+
+    private Rigidbody2D _rigidbody;
+
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        if (BadGuyLife < 0)
+        {
+            DestroyGameObject();
+        }
+
+        BadGuyLife -= Time.deltaTime;
+    }
+
+    private void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+    public void GetShoot()
+    {
+        DestroyGameObject();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Touched BadGuy D:");
+    }
+}
---
+++ Assets/Scripts/RendererController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RendererController : MonoBehaviour {
+
+    public Rigidbody2D RenderedObject;
+    public int FrequentOfRender;
+    public int SpeedOfRender;
+
+    private float _time;
+    private Vector2 _renderDirection;
+
+    void Start () {
+        _time = 0;
+        _renderDirection = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    void Update () {
+        _time++;
+        if (_time > FrequentOfRender)
+        {
+            RenderObject();
+            _time = 0;
+        }
+	}
+
+    private void RenderObject()
+    {
+        //if you want to update position of rendered objects
+        //_renderDirection = new Vector2(transform.position.x, transform.position.y);
+
+        Rigidbody2D _badGuyInstance = Instantiate(RenderedObject, _renderDirection, Quaternion.identity) as Rigidbody2D;
+        _badGuyInstance.AddForce(new Vector2(-SpeedOfRender, 0));
+    }
+}

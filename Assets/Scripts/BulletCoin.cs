5ab9d283648f01b5f16811e607875ee1ee373ce9
JotKaToJa <teronal@gmail.com>
2018-03-10 15:35:35 +0100

    Textboxs fo coins and MainScene fixes

--- Assets/Scripts/BulletCoin.cs
+++ Assets/Scripts/BulletCoin.cs
@@ -5,5 +5,6 @@ public class BulletCoin : Coin {
     {
         playerController.HasBullet = true;
         Generator.NumberOfObjects--;
+        playerController.HasBulletText.text = "Masz pocisk";
     }
 }

eeba9f96ff38f1fec0ba3fce2c9a350b09f95a02
Bartłomiej Olender <OlenderBartlomiej00@gmail.com>
2018-03-10 11:43:47 +0100

    Menu

---
+++ Assets/Scripts/ObowiązkowySkrypt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLakeJam
+{
+    public class ObowiązkowySkrypt : MonoBehaviour
+    {
+        void Start()
+        {
+            foreach (Rigidbody2D rb in FindObjectsOfType<Rigidbody2D>())
+            {
+                if (rb.GetComponent<Rigidbody2D>().gravityScale != 0)
+                {
+                    throw new UnityException("To nie przejdzie xD");
+                }
+            }
+        }
+
+    }
+}
+
